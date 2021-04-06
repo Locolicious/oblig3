3cfda54447fe0d62f749702a1196e55263debae8
Locolicious <martoandersen@hotmail.no>
2021-04-06 21:13:58 +0200

    ny

--- src/client.js
+++ src/client.js
@@ -1,8 +1,13 @@
 import sanityClient from '@sanity/client';
 
-const options = {
+/*const options = {
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: process.env.REACT_APP_SANITY_PROJECT_NAME,
+}*/
+
+const options = {
+    projectId: 'o75d9mfm',
+    dataset: 'production',
 }
 
 const client = sanityClient({
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import getMovies from '../movieService'
+import getMovies from '../movieService';
 
 
 const Movies = () => {
